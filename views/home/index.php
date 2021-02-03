731d69219e6072d32695ab24d11db52d660773dc
Ruslan <cekip@mail.ru>
2021-02-03 16:30:49 +0600

    Создал слайдеры на странице товара

--- views/home/index.php
+++ views/home/index.php
@@ -196,112 +196,112 @@
             </ul>
         </div>
     </section>
-    <section class="retailers main-block">
-        <div class="section">
-            <h2 class="title">Розничные предложения</h2>
-        </div>
-        <div class="multiple-slider retailers-slider">
-	        <a class="slide" href="#">
-                <div class="slide-inner">
-                    <div class="slide-photo"><img src="img/dveri.png" alt=""/>
-                    </div>
-                    <h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
-                    <div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
-                    </div>
-                    <div class="slide-prices">
-                        <div class="old-price">999999 ₸</div>
-                        <div class="current-price">664499 ₸</div>
-                    </div>
-                </div></a>
-	        <a class="slide" href="#">
-                <div class="slide-inner">
-                    <div class="slide-photo"><img src="img/dveri.png" alt=""/>
-                    </div>
-                    <h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
-                    <div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
-                    </div>
-                    <div class="slide-prices">
-                        <div class="old-price">999999 ₸</div>
-                        <div class="current-price">664499 ₸</div>
-                    </div>
-                </div></a>
-	        <a class="slide" href="#">
-                <div class="slide-inner">
-                    <div class="slide-photo"><img src="img/dveri.png" alt=""/>
-                    </div>
-                    <h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
-                    <div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
-                    </div>
-                    <div class="slide-prices">
-                        <div class="old-price">999999 ₸</div>
-                        <div class="current-price">664499 ₸</div>
-                    </div>
-                </div></a>
-	        <a class="slide" href="#">
-                <div class="slide-inner">
-                    <div class="slide-photo"><img src="img/dveri.png" alt=""/>
-                    </div>
-                    <h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
-                    <div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
-                    </div>
-                    <div class="slide-prices">
-                        <div class="old-price">999999 ₸</div>
-                        <div class="current-price">664499 ₸</div>
-                    </div>
-                </div></a>
-	        <a class="slide" href="#">
-                <div class="slide-inner">
-                    <div class="slide-photo"><img src="img/dveri.png" alt=""/>
-                    </div>
-                    <h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
-                    <div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
-                    </div>
-                    <div class="slide-prices">
-                        <div class="old-price">999999 ₸</div>
-                        <div class="current-price">664499 ₸</div>
-                    </div>
-                </div></a>
-	        <a class="slide" href="#">
-                <div class="slide-inner">
-                    <div class="slide-photo"><img src="img/dveri.png" alt=""/>
-                    </div>
-                    <h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
-                    <div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
-                    </div>
-                    <div class="slide-prices">
-                        <div class="old-price">999999 ₸</div>
-                        <div class="current-price">664499 ₸</div>
-                    </div>
-                </div></a>
-	        <a class="slide" href="#">
-                <div class="slide-inner">
-                    <div class="slide-photo"><img src="img/dveri.png" alt=""/>
-                    </div>
-                    <h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
-                    <div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
-                    </div>
-                    <div class="slide-prices">
-                        <div class="old-price">999999 ₸</div>
-                        <div class="current-price">664499 ₸</div>
-                    </div>
-                </div></a>
-        </div>
-        <div class="section">
-            <div class="multiple-slider-control">
-                <button class="multiple-slider-arrow retailers-prev">
-                    <svg width="53" height="19" viewBox="0 0 53 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M52.3211 9.35676H0.999999M0.999999 9.35676L10.4431 0.5448M0.999999 9.35676L10.4431 18.1687"></path>
-                    </svg>
-                </button>
-	              <div class="multiple-slider-dots"></div>
-                <button class="multiple-slider-arrow retailers-next">
-                    <svg width="53" height="19" viewBox="0 0 53 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0.178864 9.35676H51.5M51.5 9.35676L42.0569 0.5448M51.5 9.35676L42.0569 18.1687"></path>
-                    </svg>
-                </button>
-            </div>
-        </div>
-    </section>
+		<section class="retailers main-block">
+			<div class="section">
+				<h2 class="title">Розничные предложения</h2>
+			</div>
+			<div class="multiple-slider retailers-slider">
+				<a class="slide" href="#">
+					<div class="slide-inner">
+						<div class="slide-photo"><img src="img/dveri.png" alt=""/>
+						</div>
+						<h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
+						<div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
+						</div>
+						<div class="slide-prices">
+							<div class="old-price">999999 ₸</div>
+							<div class="current-price">664499 ₸</div>
+						</div>
+					</div></a>
+				<a class="slide" href="#">
+					<div class="slide-inner">
+						<div class="slide-photo"><img src="img/dveri.png" alt=""/>
+						</div>
+						<h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
+						<div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
+						</div>
+						<div class="slide-prices">
+							<div class="old-price">999999 ₸</div>
+							<div class="current-price">664499 ₸</div>
+						</div>
+					</div></a>
+				<a class="slide" href="#">
+					<div class="slide-inner">
+						<div class="slide-photo"><img src="img/dveri.png" alt=""/>
+						</div>
+						<h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
+						<div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
+						</div>
+						<div class="slide-prices">
+							<div class="old-price">999999 ₸</div>
+							<div class="current-price">664499 ₸</div>
+						</div>
+					</div></a>
+				<a class="slide" href="#">
+					<div class="slide-inner">
+						<div class="slide-photo"><img src="img/dveri.png" alt=""/>
+						</div>
+						<h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
+						<div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
+						</div>
+						<div class="slide-prices">
+							<div class="old-price">999999 ₸</div>
+							<div class="current-price">664499 ₸</div>
+						</div>
+					</div></a>
+				<a class="slide" href="#">
+					<div class="slide-inner">
+						<div class="slide-photo"><img src="img/dveri.png" alt=""/>
+						</div>
+						<h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
+						<div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
+						</div>
+						<div class="slide-prices">
+							<div class="old-price">999999 ₸</div>
+							<div class="current-price">664499 ₸</div>
+						</div>
+					</div></a>
+				<a class="slide" href="#">
+					<div class="slide-inner">
+						<div class="slide-photo"><img src="img/dveri.png" alt=""/>
+						</div>
+						<h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
+						<div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
+						</div>
+						<div class="slide-prices">
+							<div class="old-price">999999 ₸</div>
+							<div class="current-price">664499 ₸</div>
+						</div>
+					</div></a>
+				<a class="slide" href="#">
+					<div class="slide-inner">
+						<div class="slide-photo"><img src="img/dveri.png" alt=""/>
+						</div>
+						<h4 class="slide-title">Противопожарные входные двери</h4><span class="slide-code">DR-84389-94</span>
+						<div class="slide-icon"><img src="img/fire-shield.svg" alt=""/><span></span>
+						</div>
+						<div class="slide-prices">
+							<div class="old-price">999999 ₸</div>
+							<div class="current-price">664499 ₸</div>
+						</div>
+					</div></a>
+			</div>
+			<div class="section">
+				<div class="multiple-slider-control">
+					<button class="multiple-slider-arrow retailers-prev">
+						<svg width="53" height="19" viewBox="0 0 53 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M52.3211 9.35676H0.999999M0.999999 9.35676L10.4431 0.5448M0.999999 9.35676L10.4431 18.1687"></path>
+						</svg>
+					</button>
+					<div class="multiple-slider-dots"></div>
+					<button class="multiple-slider-arrow retailers-next">
+						<svg width="53" height="19" viewBox="0 0 53 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0.178864 9.35676H51.5M51.5 9.35676L42.0569 0.5448M51.5 9.35676L42.0569 18.1687"></path>
+						</svg>
+					</button>
+				</div>
+			</div>
+		</section>
     <section class="corp main-block">
         <div class="section">
             <div class="corp-info">
